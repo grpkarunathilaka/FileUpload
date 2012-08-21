e06f111adc2a672a714b0024ef8c49a44025ce95
Ranga Gamage <grpkarunathilaka@gmail.com>
2012-08-21 22:25:09 +0530

    Initial Project include

---
+++ FileUploader/FileUploader/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public string Upload(HttpPostedFileBase fileData)
+        {
+            var fileName = this.Server.MapPath("~/uploads/" + System.IO.Path.GetFileName(fileData.FileName));
+            fileData.SaveAs(fileName);
+
+            return "ok";
+        }
+
+    }
+}
